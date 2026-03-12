c827072e5239edb52437afcff409e957dda04817
adrianroguez <sterbenthereaper@gmail.com>
2026-03-12 19:53:51 +0000

    Se mejora visualmente el login y el register #9

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,17 +1,27 @@
 import { Slot } from "expo-router";
 import React from "react";
 import { AuthProvider } from "../context/AuthContext";
-
 import { CreatureProvider } from "../context/CreatureContext";
+import { useFonts, PressStart2P_400Regular } from "@expo-google-fonts/press-start-2p";
+import * as SplashScreen from "expo-splash-screen";
 
-/**
- * Componente de layout raiz de la aplicacion.
- * Envuelve toda la app con los proveedores de contexto de autenticacion y criatura.
- * Esto asegura que todos los componentes hijos tengan acceso al estado de auth y criatura.
- * 
- * @returns {JSX.Element} El layout raiz con proveedores anidados
- */
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [fontsLoaded, fontError] = useFonts({
+    'PressStart2P': PressStart2P_400Regular,
+  });
+
+  React.useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <AuthProvider>
       <CreatureProvider>
